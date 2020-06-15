560b72c0a065071ef45bd2f63c2efaa99134c6f3
Ezra Black <dev.ezrablack@gmail.com>
2020-06-15 11:40:23 -0500

    added model controller and header for objC Swift interior

---
+++ Contacts-Swift-Hybrid/Contacts-Hybrid/CNSContactController.h
@@ -0,0 +1,17 @@
+//
+//  CNSContactController.h
+//  Contacts-Hybrid
+//
+//  Created by Ezra Black on 6/15/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CNSContactController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Contacts-Swift-Hybrid/Contacts-Hybrid/CNSContactController.m
@@ -0,0 +1,13 @@
+//
+//  CNSContactController.m
+//  Contacts-Hybrid
+//
+//  Created by Ezra Black on 6/15/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import "CNSContactController.h"
+
+@implementation CNSContactController
+
+@end
